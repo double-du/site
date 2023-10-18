807d2c9439d1f19004e375d13c9c436271c23eab
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-10-18 12:02:44 -0300

    fix: ajustando send.php

--- enviar.php
+++ enviar.php
@@ -45,13 +45,14 @@ $mail->Subject = 'Teste de envio';
 
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+// $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+$mail->msgHTML('works');
 
 //Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
+// $mail->AltBody = 'This is a plain-text message body';
 
 //Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
+// $mail->addAttachment('images/phpmailer_mini.png');
 
 //send the message, check for errors
 if (!$mail->send()) {
