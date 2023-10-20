e7c250180ae0a9282bf9c4515a905092c497feeb
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-10-20 17:38:27 -0300

    feat: adicionando funções assincronas

--- assets/js/envia_email.js
+++ assets/js/envia_email.js
@@ -1,7 +1,17 @@
-botao = document.querySelector('.contato__button');
+const botao = document.querySelector('.contato__button')
+const resposta = document.querySelector('.resposta')
+
+let nome = document.querySelector('.nome')
+let email = document.querySelector('.email')
+let mensagem = document.querySelector('.telefone')
 
 async function envia_emails(){
-    
+    const url = '/envia.php?nome=' + nome + '&email=' + email + '&msg=' + mensagem
+    const resp = await fetch(url)
+    const json = await resp.json()
+    if(json.works){
+        resposta.innerHTML = 'Obrigado pelo contato!'
+    }
 }
 
 botao.addEventListener('click', async (e) => {
