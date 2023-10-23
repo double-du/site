17e41d8ae2f6a163ac63a25a0397e930e386277e
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-10-23 07:58:44 -0300

    fix: ajustando escopo de variáveis

--- assets/js/envia_email.js
+++ assets/js/envia_email.js
@@ -1,11 +1,11 @@
 const botao = document.querySelector('.contato__button')
 const resposta = document.querySelector('.contato__resposta')
 
-let nome = document.querySelector('.contato__input--nome').value
-let email = document.querySelector('.contato__input--email').value
-let mensagem = document.querySelector('.contato__input--mensagem').value
-
 async function envia_emails(){
+    let nome = document.querySelector('.contato__input--nome').value
+    let email = document.querySelector('.contato__input--email').value
+    let mensagem = document.querySelector('.contato__input--mensagem').value
+
     const url = '/enviar.php?nome=' + nome + '&email=' + email + '&msg=' + mensagem
     const resp = await fetch(url)
     const json = await resp.json()
