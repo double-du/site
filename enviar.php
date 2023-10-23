4ac43b937301a960140425fd7f825c29804130b6
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-10-23 07:53:09 -0300

    fix: ajustando erro de caracter e nomes de rementente

--- enviar.php
+++ enviar.php
@@ -11,6 +11,8 @@ $mail = new PHPMailer;
 
 //Tell PHPMailer to use SMTP
 $mail->isSMTP();
+$mail->CharSet = "UTF-8";
+
 //Enable SMTP debugging
 // 0 = off (for production use)
 // 1 = client messages
@@ -36,13 +38,13 @@ $mail->Username = "site@eduardoaf.dev.br";
 $mail->Password = "Site*102030";
 
 //Set who the message is to be sent from
-$mail->setFrom('site@eduardoaf.dev.br', 'Eduardo Augusto Furlaneti');
+$mail->setFrom('site@eduardoaf.dev.br', 'Eduardo Roboto');
 
 //Set an alternative reply-to address
 $mail->addReplyTo($email, $nome);
 
 //Set who the message is to be sent to
-$mail->addAddress('site@eduardoaf.dev.br', 'Eduardo Roboto');
+$mail->addAddress('site@eduardoaf.dev.br', 'Eduardo de Carne e Osso');
 
 //Set the subject line
 $mail->Subject = 'Contato via Site';
@@ -51,9 +53,9 @@ $mail->Subject = 'Contato via Site';
 //convert HTML into a basic plain-text alternative body
 // $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
 $mail->msgHTML("<p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>Oi Edu!</p>
-<p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>Você recebeu um novo contato de <b>$nome</b> pelo email: <i>$email</i></p>
+<p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>Você recebeu um novo contato de <b>$_GET[nome]</b> pelo email: <i>$_GET[email]</i></p>
 <p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>Ele disse:</p>
-<p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>\"<i>$msg</i>\"</p>
+<p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>\"<i>$_GET[msg]</i>\"</p>
 <p style='font-size: 18px; font-family: sans-serif; color: #090909; line-height: 22px'>Ao responder esse e-mail, ele será enviado diretamente para $nome</p>");
 
 //Replace the plain text body with one created manually
