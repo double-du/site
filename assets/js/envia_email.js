be0d9c0f18d14704c54833770004dacb99492777
Eduardo Furlaneti <eduardohppa@gmail.com>
2023-10-23 07:55:50 -0300

    fix: ajustando erro de valor no campo

--- assets/js/envia_email.js
+++ assets/js/envia_email.js
@@ -1,9 +1,9 @@
 const botao = document.querySelector('.contato__button')
 const resposta = document.querySelector('.contato__resposta')
 
-let nome = document.querySelector('.contato__input--nome')
-let email = document.querySelector('.contato__input--email')
-let mensagem = document.querySelector('.contato__input--mensagem')
+let nome = document.querySelector('.contato__input--nome').value
+let email = document.querySelector('.contato__input--email').value
+let mensagem = document.querySelector('.contato__input--mensagem').value
 
 async function envia_emails(){
     const url = '/enviar.php?nome=' + nome + '&email=' + email + '&msg=' + mensagem
