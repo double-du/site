ec437d05429f80b71fa1f334c4693a6c3aedf314
Eduardo Augusto Furlaneti <eduardohppa@gmail.com>
2025-05-29 02:11:45 -0300

    Adding translating - adjusting directory hierarchy

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php include 'lang/lang.php'; ?>
 <!DOCTYPE html>
 <html lang="<?= $lang ?>">
+    <!--
+        <?= var_dump($lang); ?>
+    -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- lang/pt.php
+++ lang/pt.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'home' => 'Início',
+    'home' => 'Home',
     'skills' => 'Habilidades',
     'portfolio' => 'Portfólio',
     'contact' => 'Contato',
