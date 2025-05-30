6de9e62453e4ebed1d6779cfdb82dce0b9b792c9
Eduardo Furlaneti <eduardo.furlaneti@agendor.com.br>
2025-05-29 23:13:52 -0300

    removing comment

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php include 'lang/lang.php'; ?>
 <!DOCTYPE html>
 <html lang="<?= $lang ?>">
-    <!--
-        <?= var_dump($t); ?>
-    -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
